451930e92e646bcaa053b3ccd2c114a7daa1878b
Andrew C <andrew.cranston@gmail.com>
2025-10-13 17:37:10 +1100

    Evaluate distance expressions with basic arithmetic

--- modules/nModules/nShared/Distance.cpp
+++ modules/nModules/nShared/Distance.cpp
@@ -7,8 +7,250 @@
 //-------------------------------------------------------------------------------------------------
 #include "Distance.hpp"
 
+#include <cmath>
+#include <cwctype>
 #include <stdlib.h>
 
+namespace
+{
+    struct DistanceComponents
+    {
+        float pixels = 0.0f;
+        float percent = 0.0f;
+    };
+
+    class DistanceExpressionParser
+    {
+    public:
+        explicit DistanceExpressionParser(const wchar_t* expression)
+            : m_cursor(expression)
+        {
+        }
+
+        bool Parse(DistanceComponents& out)
+        {
+            SkipWhitespace();
+            if (*m_cursor == L'\0')
+            {
+                return false;
+            }
+
+            if (!ParseSum(out))
+            {
+                return false;
+            }
+
+            SkipWhitespace();
+            return (*m_cursor == L'\0');
+        }
+
+    private:
+        static bool IsZero(float value)
+        {
+            return std::fabs(value) < 0.0001f;
+        }
+
+        void SkipWhitespace()
+        {
+            while (*m_cursor != L'\0' && iswspace(*m_cursor))
+            {
+                ++m_cursor;
+            }
+        }
+
+        bool ParseSum(DistanceComponents& out)
+        {
+            if (!ParseProduct(out))
+            {
+                return false;
+            }
+
+            while (true)
+            {
+                SkipWhitespace();
+                if (*m_cursor == L'+' || *m_cursor == L'-')
+                {
+                    const wchar_t op = *m_cursor++;
+                    DistanceComponents rhs;
+                    if (!ParseProduct(rhs))
+                    {
+                        return false;
+                    }
+
+                    if (op == L'+')
+                    {
+                        out.pixels += rhs.pixels;
+                        out.percent += rhs.percent;
+                    }
+                    else
+                    {
+                        out.pixels -= rhs.pixels;
+                        out.percent -= rhs.percent;
+                    }
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            return true;
+        }
+
+        bool ParseProduct(DistanceComponents& out)
+        {
+            if (!ParseFactor(out))
+            {
+                return false;
+            }
+
+            while (true)
+            {
+                SkipWhitespace();
+                if (*m_cursor == L'*' || *m_cursor == L'/')
+                {
+                    const wchar_t op = *m_cursor++;
+                    DistanceComponents rhs;
+                    if (!ParseFactor(rhs))
+                    {
+                        return false;
+                    }
+
+                    if (op == L'*')
+                    {
+                        if (!Multiply(out, rhs))
+                        {
+                            return false;
+                        }
+                    }
+                    else
+                    {
+                        if (!Divide(out, rhs))
+                        {
+                            return false;
+                        }
+                    }
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            return true;
+        }
+
+        bool ParseFactor(DistanceComponents& out)
+        {
+            SkipWhitespace();
+
+            bool negate = false;
+            while (*m_cursor == L'+' || *m_cursor == L'-')
+            {
+                if (*m_cursor == L'-')
+                {
+                    negate = !negate;
+                }
+                ++m_cursor;
+                SkipWhitespace();
+            }
+
+            if (*m_cursor == L'(')
+            {
+                ++m_cursor;
+                if (!ParseSum(out))
+                {
+                    return false;
+                }
+
+                SkipWhitespace();
+                if (*m_cursor != L')')
+                {
+                    return false;
+                }
+                ++m_cursor;
+            }
+            else
+            {
+                const wchar_t* start = m_cursor;
+                float value = wcstof(m_cursor, const_cast<wchar_t**>(&m_cursor));
+                if (m_cursor == start)
+                {
+                    return false;
+                }
+
+                SkipWhitespace();
+
+                if (*m_cursor == L'%')
+                {
+                    ++m_cursor;
+                    out.pixels = 0.0f;
+                    out.percent = value / 100.0f;
+                }
+                else if (StartsWithInsensitive(L"dip"))
+                {
+                    m_cursor += 3;
+                    out.pixels = value;
+                    out.percent = 0.0f;
+                }
+                else
+                {
+                    out.pixels = value;
+                    out.percent = 0.0f;
+                }
+            }
+
+            if (negate)
+            {
+                out.pixels = -out.pixels;
+                out.percent = -out.percent;
+            }
+
+            return true;
+        }
+
+        bool Multiply(DistanceComponents& lhs, const DistanceComponents& rhs)
+        {
+            if (!IsZero(lhs.percent) && !IsZero(rhs.percent))
+            {
+                return false;
+            }
+
+            DistanceComponents result;
+            result.pixels = lhs.pixels * rhs.pixels;
+            result.percent = lhs.pixels * rhs.percent + rhs.pixels * lhs.percent;
+
+            lhs = result;
+            return true;
+        }
+
+        bool Divide(DistanceComponents& lhs, const DistanceComponents& rhs)
+        {
+            if (!IsZero(rhs.percent))
+            {
+                return false;
+            }
+
+            const float divisor = rhs.pixels;
+            if (IsZero(divisor))
+            {
+                return false;
+            }
+
+            lhs.pixels /= divisor;
+            lhs.percent /= divisor;
+            return true;
+        }
+
+        bool StartsWithInsensitive(LPCWSTR token)
+        {
+            const size_t len = wcslen(token);
+            return _wcsnicmp(m_cursor, token, len) == 0;
+        }
+
+        const wchar_t* m_cursor;
+    };
+} // namespace
 
 Distance::Distance() {}
 
@@ -75,34 +317,19 @@ float Distance::Evaluate(float parentLength /*, float dpi*/) {
 /// </param>
 /// <return>true if the string is valid related number.</return>
 bool Distance::Parse(LPCWSTR distanceString, Distance &out) {
-  if (distanceString == nullptr || *distanceString == L'\0') {
+  if (distanceString == nullptr) {
     return false;
   }
 
-  float pixels = 0;
-  float percentage = 0;
-  float dips = 0;
+  DistanceComponents components;
+  DistanceExpressionParser parser(distanceString);
 
-  // Look at the string as a collection of tokens, delimited by + and -
-  while (*distanceString) {
-    float number = wcstof(distanceString, const_cast<LPWSTR*>(&distanceString));
-    if (*distanceString == L'%') {
-      percentage += number;
-      ++distanceString;
-    } else if (distanceString[0] == L'd' && distanceString[1] == L'i' && distanceString[2] == L'p') {
-      dips += number;
-      distanceString += 3;
-    } else {
-      pixels += number;
-    }
-
-    // The next token has to be a +, -, or end of string.
-    if (*distanceString != L'+' && *distanceString != L'-' && *distanceString != L'\0') {
-      return false;
-    }
+  if (!parser.Parse(components))
+  {
+    return false;
   }
 
-  out = Distance(pixels, percentage / 100.0f /*, dips */);
+  out = Distance(components.pixels, components.percent /*, dips */);
 
   return true;
 }
